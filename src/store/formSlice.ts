e15e8f1b6f590f7adae3569620734f48d8cf7f0c
v1b3m <vibenjamin6@gmail.com>
2024-09-02 21:42:33 +0300

    Add form api

--- src/store/formSlice.ts
+++ src/store/formSlice.ts
@@ -1,4 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { TForm } from "../components/Page";
+
+// Define a service using a base URL and expected endpoints
+export const formApi = createApi({
+  reducerPath: "formApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/" }),
+  endpoints: (builder) => ({
+    getConfig: builder.query<TForm, string>({
+      query: () => `api/config`,
+    }),
+    submitForm: builder.mutation({
+      query: (form) => ({
+        url: "/api/submit",
+        method: "POST",
+        body: form,
+      }),
+    }),
+  }),
+});
+
+export const { useGetConfigQuery, useSubmitFormMutation } = formApi;
 
 export interface FormState {
   page: number;
